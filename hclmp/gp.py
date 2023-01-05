d109c2e9c189bad4a78ee8c21c573f2a2bce209d
Kevin Tran <kevin.tran@tri.global>
2023-01-05 20:32:06 +0000

    Create Sparse GP and parallel sparse GPs

--- hclmp/gp.py
+++ hclmp/gp.py
@@ -94,6 +94,335 @@ class PenultDataset(torch.utils.data.Dataset):
         self.pen_values = torch.tensor(pen_values_stacked).to(self.device)
 
 
+class SparseGP(gpytorch.models.ExactGP):
+    """
+    A Gaussian Process meant to be trained in batches using a set of inducing
+    points for the covariance rather the entire training set.
+    """
+
+    def __init__(self,
+                 train_inputs: torch.tensor,
+                 train_targets: torch.tensor,
+                 likelihood: gpytorch.likelihoods.Likelihood,
+                 mean_module: gpytorch.means.Mean,
+                 base_covar_module: gpytorch.kernels.Kernel,
+                 num_inducing_points: int = 512,
+                 device: torch.device = None,
+                 ):
+        """
+
+        Args:
+            train_inputs (torch.tensor): The features to initialize this GP
+
+            train_targets (torch.tensor): The labels to initialize this GP
+
+            likelihood (gpytorch.likelihoods.Likelihood): The likelihood object
+            to train
+
+            mean_module (gpytorch.means.Mean): The mean object to use and train
+
+            base_covar_module (gpytorch.kernels.Kernel): The kernel to use and train
+
+            num_inducing_points (int): The number of inducing points to use
+
+            device (torch.device): The device that you want this model to run
+            on. If `None`, then uses any available GPU.
+        """
+
+        super().__init__(train_inputs=train_inputs,
+                         train_targets=train_targets,
+                         likelihood=likelihood)
+
+        self.device = device
+        if self.device is None:
+            self.device = (torch.device('cuda') if torch.cuda.is_available()
+                           else torch.device('cpu'))
+
+        self.mean_module = mean_module.to(self.device)
+        self.base_covar_module = base_covar_module.to(self.device)
+
+        sample_indices = np.random.choice(
+            list(range(len(train_inputs))),
+            size=num_inducing_points,
+            replace=False,
+        )
+        self.inducing_points = train_inputs[sample_indices].to(self.device)
+
+        self.covar_module = gpytorch.kernels.InducingPointKernel(
+            base_kernel=self.base_covar_module,
+            inducing_points=self.inducing_points,
+            likelihood=likelihood,
+        ).to(self.device)
+
+    def forward(self,
+                inputs: torch.tensor,
+                ) -> gpytorch.distributions.MultivariateNormal:
+        mean = self.mean_module(inputs).to(self.device)
+        covar = self.covar_module(inputs).to(self.device)
+        return gpytorch.distributions.MultivariateNormal(mean, covar)
+
+    def train_on_data(self,
+                      train_inputs: torch.tensor,
+                      train_targets: torch.tensor,
+                      n_epochs: int = 20,
+                      lr: float = 0.1,
+                      device: torch.device = None,
+                      ):
+        """
+        Train this GP on the input data given during `__init__`
+
+        Args:
+            train_inputs (torch.tensor): The features to train this GP
+
+            train_targets (torch.tensor): The labels to train this GP
+
+            n_epochs (int): The number of epochs that should be used to train
+            these models
+
+            lr (float): The learning rate to use during training
+
+            device (torch.device): The device that you want this model to run
+            on. If `None`, then uses the device given during `__init__`
+
+        Yields:
+            float: The loss after each training epoch
+        """
+
+        device = device if device is not None else self.device
+
+        # Initialize training
+        self.train()
+        self.likelihood.train()
+
+        optimizer = torch.optim.Adam(self.parameters(), lr=lr)
+        mll = gpytorch.mlls.ExactMarginalLogLikelihood(self.likelihood,
+                                                       self).to(device)
+
+        epoch_iter = tqdm(range(n_epochs), total=n_epochs, desc='Epochs')
+        for epoch in epoch_iter:
+
+            # Train on this one batch
+            optimizer.zero_grad()
+            output = self(train_inputs)
+            loss = -mll(output, train_targets)
+            epoch_iter.set_postfix(loss=loss.item())
+            loss.backward()
+            optimizer.step()
+
+            yield float(loss)
+
+    def predict(self, inputs: torch.tensor) -> (np.ndarray, np.ndarray):
+        """
+        Make predictions using the model
+
+        Args:
+            inputs (torch.tensor): The features to use to make predictions
+
+        Returns:
+            (np.ndarray, np.ndarray): The mean and standard error predictions,
+            respectively
+        """
+
+        self.eval()
+        self.likelihood.eval()
+
+        with torch.no_grad():
+            preds = self(inputs)
+            means = preds.mean.cpu().numpy().squeeze()
+            stddevs = preds.stddev.cpu().numpy().squeeze()
+
+        return np.array(means), np.array(stddevs)
+
+
+class ParallelSparseGPs:
+    """
+    This class is meant to manage sparse GPs in parallel and independent of
+    each other. There should be no information sharing between them. Thus, this
+    class is created only for ease-of-use.
+    """
+
+    CHECKPOINT_NAME = 'pgp_chkpt.pth.tar'
+
+    def __init__(self,
+                 train_inputs: torch.tensor,
+                 train_targets: torch.tensor,
+                 num_inducing_points: int = 512,
+                 device: torch.device = None,
+                 ):
+        """
+
+        Args:
+            train_inputs (torch.tensor): The features to train on
+
+            train_targets (torch.tensor): The labels to train on
+
+            num_inducing_points (int): The number of inducing points to use
+
+            device (torch.device): The device that you want this model to run
+            on. If `None`, then uses any available GPU.
+        """
+
+        self.train_inputs = train_inputs
+        self.train_targets = train_targets
+
+        self.device = device
+        if self.device is None:
+            self.device = (torch.device('cuda') if torch.cuda.is_available()
+                           else torch.device('cpu'))
+
+        n_models = train_targets.shape[-1]
+        self.gps = []
+
+        for idx in range(n_models):
+            inputs = train_inputs[:, idx, :]
+            outputs = train_targets[:, idx]
+
+            # Outputs are transmittance values. We can add a mean prior based
+            # on training data, and we can also add a constraint between [0, 1]
+            mean_prior = gpytorch.priors.NormalPrior(
+                loc=outputs.mean(), scale=outputs.std())
+            mean_constraint = gpytorch.constraints.constraints.Interval(
+                lower_bound=0, upper_bound=1)
+            mean_module = gpytorch.means.ConstantMean(
+                constant_prior=mean_prior,
+                constant_constraint=mean_constraint,
+            )
+
+            covar_module = gpytorch.kernels.ScaleKernel(
+                gpytorch.kernels.MaternKernel(nu=0.5)
+            )
+
+            likelihood = gpytorch.likelihoods.GaussianLikelihood().to(device)
+            # Set the noise to be constant (i.e., don't train on the noise).
+            # Some small value, but don't make it too small or numerical
+            # performance will suffer.
+            # likelihood.noise = 1e-2
+            # likelihood.raw_noise.requires_grad_(False)
+
+            gp = hclmp.gp.SparseGP(
+                train_inputs=inputs,
+                train_targets=outputs,
+                likelihood=likelihood,
+                mean_module=mean_module,
+                base_covar_module=covar_module,
+                num_inducing_points=num_inducing_points,
+                device=self.device
+            )
+            self.gps.append(gp)
+
+    def train_gps(self,
+                  train_inputs: torch.tensor,
+                  train_targets: torch.tensor,
+                  n_epochs: int = 20,
+                  lr: float = 0.1,
+                  device: torch.device = None,
+                  ):
+        """
+        Train all of the GPs
+
+        Args:
+            train_inputs (torch.tensor): The features to train on
+
+            train_targets (torch.tensor): The labels to train on
+
+            n_epochs (int): The number of epochs that should be used to train
+            these models
+
+            lr (float): The learning rate to use during training
+
+            device (torch.device): The device (e.g., CPU or GPU) that should be
+            used during training. If `None` will default to cuda (if
+            available).
+        """
+
+        # If we're in a notebook, we'll use a dynamic plot of the loss.  But if
+        # we're in a normal python thread, then we'll just use tqdm and print
+        # statements.
+        notebook = hclmp.core.is_notebook()
+        if notebook:
+            fig = plt.figure()
+            ax = fig.subplots(1, 1)
+            from IPython.display import display, clear_output
+
+        for gp_idx, gp in enumerate(self.gps):
+            train_inputs_slice = train_inputs[:, gp_idx, :]
+            train_targets_slice = train_targets[:, gp_idx]
+
+            epochs = []
+            losses = []
+            with gpytorch.settings.cholesky_jitter(1e-3):
+                for loss in gp.train_on_data(
+                        train_inputs=train_inputs_slice,
+                        train_targets=train_targets_slice,
+                        n_epochs=n_epochs,
+                        lr=lr,
+                        device=device,
+                 ):
+                    losses.append(loss)
+                    epoch = epochs[-1] + 1 if epochs else 0
+                    epochs.append(epoch)
+
+                    if notebook:
+                        ax.clear()
+                        ax.set_xlim([0, n_epochs])
+                        min_loss = min(0, min(losses))
+                        max_loss = max(losses)
+                        ax.set_ylim([min_loss, max_loss])
+                        ax.set_xlabel('Epoch #')
+                        ax.set_ylabel('Loss')
+                        title = f'Training GP {gp_idx}+1 of {len(self.gps)}'
+                        ax.set_title(title)
+                        sns.lineplot(x=epochs, y=losses, ax=ax)
+                        display(fig)
+                        clear_output(wait=True)
+
+    def save_state_dict(self):
+        """
+        Standardized way of saving the state dictionaries of the various
+        parallel GPs
+        """
+
+        checkpoint = {}
+        for idx, gp in enumerate(self.gps):
+            checkpoint[idx] = gp.state_dict()
+        torch.save(checkpoint, self.CHECKPOINT_NAME)
+
+    def load_state_dict(self):
+        """
+        Standardized way of loading the state dictionaries we saved via the
+        `save_state_dicts` method
+        """
+
+        checkpoint = torch.load(self.CHECKPOINT_NAME)
+        for idx, gp in enumerate(self.gps):
+            gp.load_state_dict(checkpoint[idx])
+
+    def predict(self, inputs: torch.tensor) -> (np.ndarray, np.ndarray):
+        """
+        Make predictions on the models
+
+        Args:
+            inputs (torch.tensor): The features to use to make predictions
+
+        Returns:
+            (np.ndarray, np.ndarray): The mean and standard error predictions,
+            respectively
+        """
+
+        means, stddevs = [], []
+        with torch.no_grad():
+            for gp_idx, gp in enumerate(self.gps):
+                inputs_slice = inputs[:, gp_idx, :]
+
+                means_slice, stddevs_slice = gp.predict(inputs_slice)
+                means.append(means_slice)
+                stddevs.append(stddevs_slice)
+
+        means = np.vstack(means).transpose()
+        stddevs = np.vstack(stddevs).transpose()
+        return means, stddevs
+
+
 class MultiTaskSVGP(gpytorch.models.ApproximateGP):
     """
     Multi-task/objective, sparse, variational Gaussian process. This code is
@@ -126,7 +455,7 @@ class MultiTaskSVGP(gpytorch.models.ApproximateGP):
             latent functions.
 
             device (torch.device): The device that you want this model to run
-            on. If `None`, then uses any available GP.
+            on. If `None`, then uses any available GPU.
         """
 
         self.inducing_points = inducing_points
@@ -207,7 +536,7 @@ class MultiTaskSVGP(gpytorch.models.ApproximateGP):
             num_tasks=self.num_tasks,
             num_latents=self.num_latents,
             latent_dim=-1,
-            # task_dim=-1,
+            # task_dim=-1,  # for independent multitask var strat
         ).to(self.device)
         return var_strat
 
